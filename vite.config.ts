1a97d22d7bd19e7354971916257969e526a41387
uNathi2 <Nkosinathititus1@outlook.com>
2026-06-10 18:00:13 +0200

    additions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  base:'IM_Website-Porfolio-Nkosinathi-Titus',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
